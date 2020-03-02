2ef17328266d37b995ccbd5ead1816c3f2bd425b
Alexander Linné <21100826+lajne@users.noreply.github.com>
2020-03-02 14:23:36 +0100

    convert bool values to strings

---
+++ CSharp/Convert boolean values to strings/main.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    if(word) 
+    {
+      return "Yes";
+    }
+    return "No";
+  }
+}
