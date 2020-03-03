320895bfe4b4cac3ac731f24f21801bd0e224f3e
Alexander Linné <21100826+lajne@users.noreply.github.com>
2020-03-03 11:35:12 +0100

    sum of the first nth term of series

---
+++ CSharp/Sum of the first nth term of Series/main.cs
@@ -0,0 +1,30 @@
+// Task:
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:
+// SeriesSum(1) => 1 = "1.00"
+// SeriesSum(2) => 1 + 1/4 = "1.25"
+// SeriesSum(5) => 1 + 1/4 + 1/7 + 1/10 + 1/13 = "1.57"
+// NOTE: In PHP the function is called series_sum().
+
+public class NthSeries {
+	
+	public static string seriesSum (int n) {
+    if(n == 0)
+      return "0.00";
+    
+		double sum = 1;
+    for(double i = 1, k = 4; i <  n; k = k + 3, i++) {
+      sum += 1 / k;
+    }
+    return sum.ToString(("F" + 2));
+	}
+}
