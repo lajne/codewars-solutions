9a4d6085890cb467b415f22514d4a99c56abb753
Alexander Linné <21100826+lajne@users.noreply.github.com>
2020-03-05 00:51:52 +0100

    remove the minimum

---
+++ CSharp/Remove the minimum/main.cs
@@ -0,0 +1,31 @@
+// The museum of incredible dull things
+// The museum of incredible dull things wants to get rid of some exhibitions. 
+// Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. 
+// She gives them a rating, and then removes the one with the lowest rating.
+
+// However, just as she finished rating all exhibitions, she's off to an important fair, 
+// so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. 
+//If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+// Examples
+// Remover.RemoveSmallest(new List<int>{1,2,3,4,5}) = new List<int>{2,3,4,5}
+// Remover.RemoveSmallest(new List<int>{5,3,2,1,4}) = new List<int>{5,3,2,4}
+// Remover.RemoveSmallest(new List<int>{2,2,1,2,1}) = new List<int>{2,2,2,1}
+
+public class Remover {
+  
+  public static List<int> RemoveSmallest(List<int> numbers) {
+    for(int i = 0; i < numbers.Count; i++) {
+      if(numbers[i] == numbers.Min()) {
+        numbers.RemoveAt(i);
+        break;
+      }
+    }
+    return numbers;
+  }
+}
