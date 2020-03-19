0f2ce8d7c5770fa843de70ca7882e52d40cc4ebe
Alexander Linné <21100826+lajne@users.noreply.github.com>
2020-03-19 18:43:06 +0100

    OOP

---
+++ CSharp/Object Oriented Piracy/main.cs
@@ -0,0 +1,14 @@
+public class Ship {
+
+  public int Draft;
+  public int Crew;
+  
+  public Ship(int draft, int crew) {
+    Draft = draft;
+    Crew = crew;
+  }
+  
+  public bool IsWorthIt() {
+    return this.Draft - (this.Crew * 1.5) > 20; 
+  }
+}
