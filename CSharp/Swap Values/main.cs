2b3531119aaedd0e893540c7930757ac310fa41e
Alexander Linné <21100826+lajne@users.noreply.github.com>
2020-03-23 09:02:43 +0100

    swap values

---
+++ CSharp/Swap Values/main.cs
@@ -0,0 +1,13 @@
+public class Swapper {
+    public object[] Arguments { get; private set; }
+    
+    public Swapper(object[] args) {
+        Arguments = args;
+    }
+    
+    public void SwapValues() {
+        object temp = Arguments[0];
+        Arguments[0] = Arguments[1];
+        Arguments[1] = temp;
+    }
+}
